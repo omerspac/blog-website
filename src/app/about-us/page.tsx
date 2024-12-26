168d3e3fb0197737bbe31959a546e6de86b11b2e
omerspac <omerspac@gmail.com>
2024-12-26 16:54:53 +0500

    c6

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
       <hr />
       <div className="about-div slide-in-right">
         <Image src={omerPic} alt="omer" className="about-image"/>
-        <p className="about-text">I am Muhammad Omer and I am currently pursuing a Bachelor's Degree In Computer Science. I am an Expert 2D & 3D Artist/Graphics Designer with 2 years of experience. I have completed Full-Stack Web Development Diploma from Aptech and I am currently completing my Advanced Diploma In Graphics Designing from Arena Multimedia. Additionally, I am a student of Meta, Web 3.0, AI Course in Governor House. I also work as a freelancer.</p>
+        <p className="about-text p-[30px] color-[#2a2d34] text-[20px]">I am Muhammad Omer and I am currently pursuing a Bachelor's Degree In Computer Science. I am an Expert 2D & 3D Artist/Graphics Designer with 2 years of experience. I have completed Full-Stack Web Development Diploma from Aptech and Advanced Diploma In Graphics Designing from Arena Multimedia. Additionally, I am a student of Meta, Web 3.0, AI Course in Governor House. I also work as a freelancer.</p>
       </div>
       </>
     );
